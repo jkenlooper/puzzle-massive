71d0f363f43f57e1116cdd57814deb1702c907fb
Jake Hickenlooper <jake@weboftomorrow.com>
2019-01-30 05:52:02 -0700

    Update webpack config

--- bin/puzzlectl.sh
+++ bin/puzzlectl.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
 
 COMMAND=$1
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,138 +11,148 @@ const srcEntry = require('./src/index.js')
 
 process.traceDeprecation = true
 
-module.exports = function makeWebpackConfig () {
+/**
+ * Config
+ * Reference: http://webpack.github.io/docs/configuration.html
+ * This is the object where all configuration gets set
+ */
+const config = {}
 
-  /**
-   * Config
-   * Reference: http://webpack.github.io/docs/configuration.html
-   * This is the object where all configuration gets set
-   */
-  var config = {}
+config.mode = 'development'
 
-  config.mode = 'development'
+config.bail = true
 
-  config.bail = true
+/**
+ * Entry
+ * Reference: http://webpack.github.io/docs/configuration.html#entry
+ */
+config.entry = srcEntry
 
-  /**
-   * Entry
-   * Reference: http://webpack.github.io/docs/configuration.html#entry
-   */
-  config.entry = srcEntry
+/**
+ * Output
+ * Reference: http://webpack.github.io/docs/configuration.html#output
+ */
+config.output = {
+  path: path.resolve(__dirname, 'dist'),
+  filename: '[name].bundle.js'
+}
 
-  /**
-   * Output
-   * Reference: http://webpack.github.io/docs/configuration.html#output
-   */
-  config.output = {
-    path: __dirname + '/dist',
-    filename: '[name].bundle.js'
-  }
+config.resolve = {
+  modules: ['src', 'node_modules']
+}
 
-  config.resolve = {
-    modules: ['src', 'node_modules']
-  }
+config.externals = {
+  'webcomponents.js': 'WebComponents',
+  'angular': 'angular',
+  'lazysizes': 'lazysizes',
+  'slab-massive.js': 'slabMassive',
+  'hammerjs': 'Hammer',
+  'reqwest': 'reqwest'
+}
 
-  config.externals = {
-    'webcomponents.js': 'WebComponents',
-    'angular': 'angular',
-    'lazysizes': 'lazysizes',
-    'slab-massive.js': 'slabMassive',
-    'hammerjs': 'Hammer',
-    'reqwest': 'reqwest'
-  }
-
-  config.module = {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules|\.min\.js/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              'env'
+config.module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules|\.min\.js/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            'env'
+          ]
+        }
+      }
+    },
+    {
+      test: /fonts\/.*\.(eot|svg|ttf|woff)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: { name: '[name].[ext]' }
+        }
+      ],
+      exclude: /node_modules/
+    },
+    {
+      test: /\.(png|gif|jpg)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: { name: '[name].[ext]' }
+        }
+      ],
+      exclude: /node_modules/
+    },
+    {
+      test: /.*sprite\.svg$/,
+      loader: 'svg-sprite-loader'
+    },
+    {
+      test: /\.css$/,
+      exclude: /node_modules|puzzle-pieces.css/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          'loader': 'postcss-loader',
+          'options': {
+            'ident': 'postcss',
+            'plugins': (loader) => [
+              postcssImport(),
+              postcssCustomMedia(),
+              postcssPresetEnv()
             ]
           }
         }
-      },
-      {
-        test: /fonts\/.*\.(eot|svg|ttf|woff)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' }
+      ]
+    },
+    {
+      test: /\.svg$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
           }
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|gif|jpg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' }
-          }
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /.*sprite\.svg$/,
-        loader: 'svg-sprite-loader'
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules|puzzle-pieces.css/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            'loader': 'postcss-loader',
-            'options': {
-              'ident': 'postcss',
-              'plugins': (loader) => [
-                postcssImport(),
-                postcssCustomMedia(),
-                postcssPresetEnv()
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]'
-            }
-          },
-          'svgo-loader'
-        ],
-        exclude: /(node_modules|fonts|.*sprite\.svg)/
-      },
-      {
-        test: /\.html$/,
-        use: 'raw-loader'
-      }
-    ]
-  }
-
-  config.plugins = [
-    new MiniCssExtractPlugin({filename: '[name].css'})
+        },
+        'svgo-loader'
+      ],
+      exclude: /(node_modules|fonts|.*sprite\.svg)/
+    },
+    {
+      test: /\.html$/,
+      use: 'raw-loader'
+    }
   ]
+}
 
-  config.optimization = {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+config.plugins = [
+  new MiniCssExtractPlugin({filename: '[name].css'})
+]
+
+config.stats = 'minimal'
+
+config.optimization = {
+  minimizer: [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true // set to true if you want JS source maps
+    }),
+    new OptimizeCSSAssetsPlugin({})
+  ]
+}
+
+module.exports = (env, argv) => {
+  if (argv.mode !== 'production') {
+    config.devtool = 'source-map'
+    config.watch = true
+    config.watchOptions = {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
   }
 
   return config
-}()
+}
