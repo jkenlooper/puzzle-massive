9b04bc4116038260dccb6271fa2c12a093326e8d
Jake Hickenlooper <jake@weboftomorrow.com>
2019-06-03 21:49:30 -0600

    Get most active puzzle for homepage

--- queries/_homepage-page-info-for-most-recent-active-puzzle.sql
+++ queries/_homepage-page-info-for-most-recent-active-puzzle.sql
@@ -8,8 +8,23 @@ round((min(CAST(p.table_width AS float), CAST(p.table_height AS float)) / max(CA
 FROM Puzzle AS p
 JOIN PuzzleFile AS pf ON (pf.puzzle = p.id)
 WHERE pf.name == 'preview_full'
+
+-- Get the most active puzzle in last 5 minutes or fall back on most recently updated one.
 AND p.puzzle_id = (
-select puzzle_id from Puzzle where permission = 0
-and status in (1, 2)
-order by m_date desc limit 1)
+    select puzzle_id from Puzzle as p1
+    join Timeline as t1 on (t1.puzzle = p1.id and t1.timestamp > datetime('now', '-5 minutes'))
+    where p1.permission = 0
+    and p1.status in (1, 2)
+    group by puzzle_id
+    order by count(t1.timestamp) desc
+    limit 1
+)
+OR p.puzzle_id = (
+    select puzzle_id from Puzzle as p1
+    where p1.permission = 0
+    and p1.status in (1, 2)
+    order by p1.m_date desc limit 1
+)
 GROUP BY p.id
+limit 1
+;
