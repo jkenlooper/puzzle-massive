1321d54b25e39dd407ac7b34d3bdff062e6d3fcf
Jake Hickenlooper <jake@weboftomorrow.com>
2019-12-08 06:51:34 -0700

    Reformat player_name_register.py

--- api/api/player_name_register.py
+++ api/api/player_name_register.py
@@ -11,14 +11,14 @@ from api.tools import normalize_name_from_display_name
 
 POINT_COST_FOR_CHANGING_NAME = 100
 
-ACTIONS = (
-    'reject',
-)
+ACTIONS = ("reject",)
+
 
 class AdminPlayerNameRegisterView(MethodView):
     """
     Handle actions on a batch of submitted names on the NameRegister.
     """
+
     def post(self):
         "Route is protected by basic auth in nginx"
         args = {}
@@ -26,11 +26,11 @@ class AdminPlayerNameRegisterView(MethodView):
             args.update(request.form.to_dict(flat=True))
 
         # Verify args
-        action = args.get('action')
+        action = args.get("action")
         if action not in ACTIONS:
             abort(400)
 
-        name_register_ids = request.form.getlist('name_register_id')
+        name_register_ids = request.form.getlist("name_register_id")
         if len(name_register_ids) == 0:
             abort(400)
         if not isinstance(name_register_ids, list):
@@ -38,31 +38,37 @@ class AdminPlayerNameRegisterView(MethodView):
 
         cur = db.cursor()
 
-        if action == 'reject':
+        if action == "reject":
+
             def each(name_register_ids):
                 for id in name_register_ids:
-                    yield {'id': id}
+                    yield {"id": id}
 
-            cur.executemany(fetch_query_string("reject-name-on-name-register-for-id.sql"), each(name_register_ids))
+            cur.executemany(
+                fetch_query_string("reject-name-on-name-register-for-id.sql"),
+                each(name_register_ids),
+            )
 
         db.commit()
         cur.close()
-        return redirect('/chill/site/admin/name-register-review/')
+        return redirect("/chill/site/admin/name-register-review/")
+
 
 class PlayerNameRegisterView(MethodView):
     """
     Handle actions for the player and any name that they own on the name register.
     """
+
     decorators = [user_not_banned]
 
     def post(self):
         ""
-        response = {
-            "message": "",
-            "name": "error"
-        }
+        response = {"message": "", "name": "error"}
 
-        user = int(current_app.secure_cookie.get(u'user') or user_id_from_ip(request.headers.get('X-Real-IP')))
+        user = int(
+            current_app.secure_cookie.get(u"user")
+            or user_id_from_ip(request.headers.get("X-Real-IP"))
+        )
         if user == None:
             response["message"] = "User not signed in."
             response["name"] = "error"
@@ -73,12 +79,12 @@ class PlayerNameRegisterView(MethodView):
         if request.form:
             args.update(request.form.to_dict(flat=True))
 
-        display_name = args.get('name', '').strip()
+        display_name = args.get("name", "").strip()
         name = normalize_name_from_display_name(display_name)
 
         # name is always converted to lowercase and display_name preserves
         # original case.
-        display_name = args.get('name', '').strip()
+        display_name = args.get("name", "").strip()
         name = normalize_name_from_display_name(display_name)
         if len(display_name) > USER_NAME_MAXLENGTH:
             response["message"] = "Submitted name is too long."
@@ -87,73 +93,109 @@ class PlayerNameRegisterView(MethodView):
 
         cur = db.cursor()
 
-        result = cur.execute(fetch_query_string("select-minimum-points-for-user.sql"), {'user': user, 'points': POINT_COST_FOR_CHANGING_NAME}).fetchone()
+        result = cur.execute(
+            fetch_query_string("select-minimum-points-for-user.sql"),
+            {"user": user, "points": POINT_COST_FOR_CHANGING_NAME},
+        ).fetchone()
         if not result:
             response["message"] = "Not enough points to change name."
             response["name"] = "error"
             cur.close()
             return make_response(json.jsonify(response), 400)
         else:
-            if name == '':
-                cur.execute(fetch_query_string('remove-user-name-on-name-register-for-player.sql'), {
-                    'player_id': user,
-                })
-                cur.execute(fetch_query_string("decrease-user-points.sql"), {
-                    "points": POINT_COST_FOR_CHANGING_NAME,
-                    "user": user,
-                })
+            if name == "":
+                cur.execute(
+                    fetch_query_string(
+                        "remove-user-name-on-name-register-for-player.sql"
+                    ),
+                    {"player_id": user,},
+                )
+                cur.execute(
+                    fetch_query_string("decrease-user-points.sql"),
+                    {"points": POINT_COST_FOR_CHANGING_NAME, "user": user,},
+                )
                 response["message"] = "Removed name."
                 response["name"] = "success"
             else:
-                result = cur.execute(fetch_query_string("check-status-of-name-on-name-register.sql"), {'name': name}).fetchall()
+                result = cur.execute(
+                    fetch_query_string("check-status-of-name-on-name-register.sql"),
+                    {"name": name},
+                ).fetchall()
                 if result:
                     (result, col_names) = rowify(result, cur.description)
                     name_status = result[0]
-                    if name_status['rejected'] == 1:
+                    if name_status["rejected"] == 1:
                         response["message"] = "Submitted name has been rejected before."
                         response["name"] = "rejected"
-                    elif name_status['claimed'] == 0 or name_status['user'] == user:
+                    elif name_status["claimed"] == 0 or name_status["user"] == user:
                         # The name is available and can be claimed. If owned by the
                         # user the casing of the letters can be modified.
-                        cur.execute(fetch_query_string('remove-user-name-on-name-register-for-player.sql'), {
-                            'player_id': user,
-                        })
+                        cur.execute(
+                            fetch_query_string(
+                                "remove-user-name-on-name-register-for-player.sql"
+                            ),
+                            {"player_id": user,},
+                        )
 
                         # Also updates the display_name if casing has changed
-                        cur.execute(fetch_query_string('claim-user-name-on-name-register-for-player.sql'), {
-                            'player_id': user,
-                            'name': name,
-                            'display_name': display_name,
-                            'time': '+1 hour',
-                        })
-                        cur.execute(fetch_query_string("decrease-user-points.sql"), {
-                            "points": POINT_COST_FOR_CHANGING_NAME,
-                            "user": user,
-                        })
-                        response["message"] = "Submitted name ({}) reclaimed.".format(display_name)
+                        cur.execute(
+                            fetch_query_string(
+                                "claim-user-name-on-name-register-for-player.sql"
+                            ),
+                            {
+                                "player_id": user,
+                                "name": name,
+                                "display_name": display_name,
+                                "time": "+1 hour",
+                            },
+                        )
+                        cur.execute(
+                            fetch_query_string("decrease-user-points.sql"),
+                            {"points": POINT_COST_FOR_CHANGING_NAME, "user": user,},
+                        )
+                        response["message"] = "Submitted name ({}) reclaimed.".format(
+                            display_name
+                        )
                         response["name"] = "success"
                     else:
-                        response["message"] = "Submitted name ({}) is currently used by another player.  Please try a different name.".format(display_name)
+                        response[
+                            "message"
+                        ] = "Submitted name ({}) is currently used by another player.  Please try a different name.".format(
+                            display_name
+                        )
                         response["name"] = "rejected"
                 else:
                     # name is new
-                    cur.execute(fetch_query_string('remove-user-name-on-name-register-for-player.sql'), {
-                        'player_id': user,
-                    })
-                    cur.execute(fetch_query_string('add-user-name-on-name-register-for-player-to-be-reviewed.sql'), {
-                        'player_id': user,
-                        'name': name,
-                        'display_name': display_name,
-                        'time': '+1 hour',
-                    })
+                    cur.execute(
+                        fetch_query_string(
+                            "remove-user-name-on-name-register-for-player.sql"
+                        ),
+                        {"player_id": user,},
+                    )
+                    cur.execute(
+                        fetch_query_string(
+                            "add-user-name-on-name-register-for-player-to-be-reviewed.sql"
+                        ),
+                        {
+                            "player_id": user,
+                            "name": name,
+                            "display_name": display_name,
+                            "time": "+1 hour",
+                        },
+                    )
 
-                    cur.execute(fetch_query_string("decrease-user-points.sql"), {
-                        "points": POINT_COST_FOR_CHANGING_NAME,
-                        "user": user,
-                    })
-                    response["message"] = "Thank you for submitting a new name.  \"{}\" will be shown next to your bit icon.".format(display_name)
+                    cur.execute(
+                        fetch_query_string("decrease-user-points.sql"),
+                        {"points": POINT_COST_FOR_CHANGING_NAME, "user": user,},
+                    )
+                    response[
+                        "message"
+                    ] = 'Thank you for submitting a new name.  "{}" will be shown next to your bit icon.'.format(
+                        display_name
+                    )
                     response["name"] = "success"
 
         db.commit()
         cur.close()
+        # TODO: purge the player-bit from cache
         return make_response(json.jsonify(response), 202)
