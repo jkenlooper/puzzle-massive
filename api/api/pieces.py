57faf6c022c20bafd800915c768eed5c9673cd89
Jake Hickenlooper <jake@weboftomorrow.com>
2019-12-13 06:31:49 -0700

    Reformat pieces.py

--- api/api/pieces.py
+++ api/api/pieces.py
@@ -17,11 +17,12 @@ from .jobs.convertPiecesToRedis import convert
 from .user import user_id_from_ip, increase_ban_time
 
 from .constants import COMPLETED
-#from jobs import pieceMove
+
+# from jobs import pieceMove
 
 encoder = json.JSONEncoder(indent=2, sort_keys=True)
 
-redisConnection = redis.from_url('redis://localhost:6379/0/', decode_responses=True)
+redisConnection = redis.from_url("redis://localhost:6379/0/", decode_responses=True)
 
 # TODO: create a puzzle status web socket that will update when the status of
 # the puzzle changes from converting, done, active, etc.
@@ -31,6 +32,7 @@ redisConnection = redis.from_url('redis://localhost:6379/0/', decode_responses=T
 PUZZLE_VIEW_RATE_TIMEOUT = 100
 PUZZLE_VIEW_MAX_COUNT = 10
 BAN_TIME_INCR_FOR_EACH = 60 * 5
+
 
 class PuzzlePiecesView(MethodView):
     """
@@ -50,7 +52,9 @@ class PuzzlePiecesView(MethodView):
 
         # TODO: optimize the timestamp used here by truncating to last digits based
         # on the expiration of the key.
-        puzzle_view_rate_key = 'pvrate:{user}:{timestamp}'.format(user=user, timestamp=rounded_timestamp)
+        puzzle_view_rate_key = "pvrate:{user}:{timestamp}".format(
+            user=user, timestamp=rounded_timestamp
+        )
         if redisConnection.setnx(puzzle_view_rate_key, 1):
             redisConnection.expire(puzzle_view_rate_key, PUZZLE_VIEW_RATE_TIMEOUT)
         else:
@@ -60,80 +64,93 @@ class PuzzlePiecesView(MethodView):
 
     def get(self, puzzle_id):
         ""
-        ip = request.headers.get('X-Real-IP')
-        user = int(current_app.secure_cookie.get(u'user') or user_id_from_ip(ip))
+        ip = request.headers.get("X-Real-IP")
+        user = int(current_app.secure_cookie.get(u"user") or user_id_from_ip(ip))
         self.bump_count(user)
 
         cur = db.cursor()
-        result = cur.execute(fetch_query_string('select_viewable_puzzle_id.sql'), {
-            'puzzle_id': puzzle_id
-            }).fetchall()
+        result = cur.execute(
+            fetch_query_string("select_viewable_puzzle_id.sql"),
+            {"puzzle_id": puzzle_id},
+        ).fetchall()
         if not result:
             # 404 if puzzle or piece does not exist
             abort(404)
 
         (result, col_names) = rowify(result, cur.description)
-        puzzle = result[0].get('puzzle')
-        status = result[0].get('status')
+        puzzle = result[0].get("puzzle")
+        status = result[0].get("status")
 
-        #TODO: if puzzle is not in redis then create a job to convert and respond with a 202 Accepted
+        # TODO: if puzzle is not in redis then create a job to convert and respond with a 202 Accepted
         # if job is already active for this request respond with 202 Accepted
 
         # Load the piece data from sqlite on demand
-        if not redisConnection.zscore('pcupdates', puzzle):
-        #if not redisConnection.exists('pc:{puzzle}:0'.format(puzzle=puzzle)):
+        if not redisConnection.zscore("pcupdates", puzzle):
+            # if not redisConnection.exists('pc:{puzzle}:0'.format(puzzle=puzzle)):
             # TODO: publish the job to the worker queue
             # Respond with 202
 
             # Check redis memory usage and create cleanup job if it's past a threshold
-            memory = redisConnection.info(section='memory')
-            print('used_memory: {used_memory_human}'.format(**memory))
-            maxmemory = memory.get('maxmemory')
+            memory = redisConnection.info(section="memory")
+            print("used_memory: {used_memory_human}".format(**memory))
+            maxmemory = memory.get("maxmemory")
             if maxmemory != 0:
-                target_memory = (maxmemory * 0.5)
-                if memory.get('used_memory') > target_memory:
+                target_memory = maxmemory * 0.5
+                if memory.get("used_memory") > target_memory:
                     # push to queue for further processing
                     job = current_app.cleanupqueue.enqueue_call(
-                        func='api.jobs.convertPiecesToDB.transferOldest', args=(target_memory,), result_ttl=0
+                        func="api.jobs.convertPiecesToDB.transferOldest",
+                        args=(target_memory,),
+                        result_ttl=0,
                     )
 
             # For now just convert as it doesn't take long
             convert(puzzle)
 
         query = """select id from Piece where (puzzle = :puzzle)"""
-        (all_pieces, col_names) = rowify(cur.execute(query, {'puzzle': puzzle}).fetchall(), cur.description)
+        (all_pieces, col_names) = rowify(
+            cur.execute(query, {"puzzle": puzzle}).fetchall(), cur.description
+        )
 
         # Create a pipe for buffering commands and disable atomic transactions
         pipe = redisConnection.pipeline(transaction=False)
 
         # The 'rotate' field is not public. It is for the true orientation of the piece.
         # The 'r' field is the mutable rotation of the piece.
-        publicPieceProperties = ('x', 'y', 'r', 's', 'w', 'h', 'b')
+        publicPieceProperties = ("x", "y", "r", "s", "w", "h", "b")
 
         for item in all_pieces:
-            piece = item.get('id')
-            pipe.hmget('pc:{puzzle}:{piece}'.format(puzzle=puzzle, piece=piece), *publicPieceProperties)
+            piece = item.get("id")
+            pipe.hmget(
+                "pc:{puzzle}:{piece}".format(puzzle=puzzle, piece=piece),
+                *publicPieceProperties
+            )
         allPublicPieceProperties = pipe.execute()
         # convert the list of lists into list of dicts.  Only want to return the public piece props.
-        pieces = [dict(list(zip(publicPieceProperties, properties))) for properties in allPublicPieceProperties]
+        pieces = [
+            dict(list(zip(publicPieceProperties, properties)))
+            for properties in allPublicPieceProperties
+        ]
         # TODO: Change piece properties to int type instead of string
         for item in all_pieces:
-            piece = item.get('id')
-            pieces[piece]['id'] = piece
+            piece = item.get("id")
+            pieces[piece]["id"] = piece
 
         pieceData = {
-                'positions': pieces,
-                'timestamp': '',
-                'mark': uuid.uuid4().hex[:3] # Used to differentiate any requests for a user session (handle double-clicking pieces)
-                }
+            "positions": pieces,
+            "timestamp": "",
+            "mark": uuid.uuid4().hex[
+                :3
+            ],  # Used to differentiate any requests for a user session (handle double-clicking pieces)
+        }
 
         cur.close()
 
         if status == COMPLETED:
             # transfer completed puzzles back out
-            print('transfer {0}'.format(puzzle))
+            print("transfer {0}".format(puzzle))
             job = current_app.cleanupqueue.enqueue_call(
-                func='api.jobs.convertPiecesToDB.transfer', args=(puzzle,), result_ttl=0
+                func="api.jobs.convertPiecesToDB.transfer", args=(puzzle,), result_ttl=0
             )
 
         return encoder.encode(pieceData)
