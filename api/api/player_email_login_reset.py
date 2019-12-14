6dc6c3e2fc74431beeb6aa6a4072c1ce9193bfe2
Jake Hickenlooper <jake@weboftomorrow.com>
2019-12-14 08:04:42 -0700

    Reformat player_email python code

--- api/api/player_email_login_reset.py
+++ api/api/player_email_login_reset.py
@@ -10,6 +10,7 @@ from api.database import rowify, fetch_query_string
 from api.user import user_id_from_ip, user_not_banned
 from api.constants import EMAIL_MAXLENGTH
 
+
 class PlayerEmailLoginResetView(MethodView):
     """
     Only allow users that are not currently signed in to reset their login by email.
@@ -19,18 +20,17 @@ class PlayerEmailLoginResetView(MethodView):
 
     def post(self):
         ""
-        response = {
-            "message": "",
-            "name": "error"
-        }
+        response = {"message": "", "name": "error"}
 
-        user = current_app.secure_cookie.get(u'user')
+        user = current_app.secure_cookie.get(u"user")
         if user:
-            response["message"] = "User currently logged in.  No need to reset the login by e-mail."
+            response[
+                "message"
+            ] = "User currently logged in.  No need to reset the login by e-mail."
             response["name"] = "error"
             return make_response(json.jsonify(response), 400)
 
-        user = user_id_from_ip(request.headers.get('X-Real-IP'))
+        user = user_id_from_ip(request.headers.get("X-Real-IP"))
         if user == None:
             response["message"] = "Shared user not currently logged in."
             response["name"] = "error"
@@ -40,7 +40,7 @@ class PlayerEmailLoginResetView(MethodView):
         if request.form:
             args.update(request.form.to_dict(flat=True))
 
-        email = args.get('email', '').strip().lower()
+        email = args.get("email", "").strip().lower()
         if len(email) > EMAIL_MAXLENGTH:
             response["message"] = "E-mail is too long."
             response["name"] = "error"
@@ -49,7 +49,10 @@ class PlayerEmailLoginResetView(MethodView):
         cur = db.cursor()
 
         # Get user by their verified email address
-        result = cur.execute(fetch_query_string('get-user-by-verified-email-address.sql'), {'email': email}).fetchone()
+        result = cur.execute(
+            fetch_query_string("get-user-by-verified-email-address.sql"),
+            {"email": email},
+        ).fetchone()
         if not result or result[0] == 0:
             cur.close()
             response["message"] = "Sorry, that e-mail address has not been verified."
@@ -58,7 +61,10 @@ class PlayerEmailLoginResetView(MethodView):
         else:
             user = result[0]
 
-        result = cur.execute(fetch_query_string('select-player-details-for-player-id.sql'), {'player_id': user}).fetchall()
+        result = cur.execute(
+            fetch_query_string("select-player-details-for-player-id.sql"),
+            {"player_id": user},
+        ).fetchall()
         if not result:
             # This shouldn't happen if user-has-player-account.sql
             cur.close()
@@ -68,10 +74,11 @@ class PlayerEmailLoginResetView(MethodView):
         (result, col_names) = rowify(result, cur.description)
         existing_player_data = result[0]
 
-
         if existing_player_data["has_active_reset_login_token"]:
             cur.close()
-            response["message"] = "Please check your e-mail for a reset login link.  The reset login link that was sent earlier has not expired."
+            response[
+                "message"
+            ] = "Please check your e-mail for a reset login link.  The reset login link that was sent earlier has not expired."
             response["name"] = "error"
             return make_response(json.jsonify(response), 400)
 
@@ -87,35 +94,50 @@ After visiting that web page and clicking the reset login button; a new login
 link will be created. Any older ones will no longer be valid.
 
 You can ignore this message if you didn't initiate the request.
-        """.format(token=token, DOMAIN_NAME=current_app.config.get('DOMAIN_NAME'))
+        """.format(
+            token=token, DOMAIN_NAME=current_app.config.get("DOMAIN_NAME")
+        )
         current_app.logger.debug(message)
         email_sent = False
-        if not current_app.config.get('DEBUG', True):
+        if not current_app.config.get("DEBUG", True):
             try:
-                send_message(email,
-                             'Puzzle Massive - reset login', message, current_app.config)
+                send_message(
+                    email, "Puzzle Massive - reset login", message, current_app.config
+                )
                 email_sent = True
             except Exception as err:
-                logger.warning("Failed to send reset login message. email: {email}\n {message}\n error: {err}".format(err=err, email=email, message=message))
+                logger.warning(
+                    "Failed to send reset login message. email: {email}\n {message}\n error: {err}".format(
+                        err=err, email=email, message=message
+                    )
+                )
                 email_sent = False
 
-        cur.execute(fetch_query_string('update-player-account-login-token.sql'), {
-            'player_id': user,
-            'reset_login_token': token,
-            'expire_token_timeout': '+1 day',
-        })
+        cur.execute(
+            fetch_query_string("update-player-account-login-token.sql"),
+            {
+                "player_id": user,
+                "reset_login_token": token,
+                "expire_token_timeout": "+1 day",
+            },
+        )
 
-        if current_app.config.get('DEBUG', True):
-            response["message"] = "A reset login link has been sent to your e-mail. DEBUG is True, so did not really send the email. Check the logs for the login link."
+        if current_app.config.get("DEBUG", True):
+            response[
+                "message"
+            ] = "A reset login link has been sent to your e-mail. DEBUG is True, so did not really send the email. Check the logs for the login link."
             response["name"] = "success"
         elif not email_sent:
-            response["message"] = "Failed to send reset login link to your e-mail.".format(email)
+            response[
+                "message"
+            ] = "Failed to send reset login link to your e-mail.".format(email)
             response["name"] = "error"
         else:
-            response["message"] = "A reset login link has been sent to your e-mail.".format(email)
+            response[
+                "message"
+            ] = "A reset login link has been sent to your e-mail.".format(email)
             response["name"] = "success"
 
         db.commit()
         cur.close()
         return make_response(json.jsonify(response), 202)
-
--- api/api/player_email_register.py
+++ api/api/player_email_register.py
@@ -14,19 +14,17 @@ from api.constants import EMAIL_MAXLENGTH
 class PlayerEmailRegisterView(MethodView):
     """
     """
+
     decorators = [user_not_banned]
 
     def post(self):
         ""
-        response = {
-            "message": "",
-            "name": "error"
-        }
+        response = {"message": "", "name": "error"}
 
         is_shareduser = False
-        user = current_app.secure_cookie.get(u'user')
+        user = current_app.secure_cookie.get(u"user")
         if not user:
-            user = user_id_from_ip(request.headers.get('X-Real-IP'))
+            user = user_id_from_ip(request.headers.get("X-Real-IP"))
             is_shareduser = True
 
         if user == None:
@@ -40,7 +38,7 @@ class PlayerEmailRegisterView(MethodView):
         if request.form:
             args.update(request.form.to_dict(flat=True))
 
-        email = args.get('email', '').strip().lower()
+        email = args.get("email", "").strip().lower()
         if len(email) > EMAIL_MAXLENGTH:
             response["message"] = "E-mail is too long."
             response["name"] = "error"
@@ -48,13 +46,19 @@ class PlayerEmailRegisterView(MethodView):
 
         cur = db.cursor()
 
-        result = cur.execute(fetch_query_string('user-has-player-account.sql'), {'player_id': user}).fetchone()
+        result = cur.execute(
+            fetch_query_string("user-has-player-account.sql"), {"player_id": user}
+        ).fetchone()
         if not result or result[0] == 0:
-            cur.execute(fetch_query_string('init-player-account-for-user.sql'), {
-                'player_id': user
-            })
+            cur.execute(
+                fetch_query_string("init-player-account-for-user.sql"),
+                {"player_id": user},
+            )
 
-        result = cur.execute(fetch_query_string('select-player-details-for-player-id.sql'), {'player_id': user}).fetchall()
+        result = cur.execute(
+            fetch_query_string("select-player-details-for-player-id.sql"),
+            {"player_id": user},
+        ).fetchall()
         if not result:
             # This shouldn't happen if user-has-player-account.sql
             cur.close()
@@ -65,42 +69,56 @@ class PlayerEmailRegisterView(MethodView):
         existing_player_data = result[0]
 
         # Prevent shareduser changing the verified email address.
-        if is_shareduser and existing_player_data['is_verifying_email']:
-            response["message"] = "A player on this same network has already submitted an email address. Changing it is not allowed until the account has been claimed or the verify email token expires."
+        if is_shareduser and existing_player_data["is_verifying_email"]:
+            response[
+                "message"
+            ] = "A player on this same network has already submitted an email address. Changing it is not allowed until the account has been claimed or the verify email token expires."
             response["name"] = "error"
             return make_response(json.jsonify(response), 400)
 
-        if existing_player_data['email'] == email:
-            response["message"] = "No changes made to e-mail address ({}).  The e-mail address is stored as lowercase.".format(email)
+        if existing_player_data["email"] == email:
+            response[
+                "message"
+            ] = "No changes made to e-mail address ({}).  The e-mail address is stored as lowercase.".format(
+                email
+            )
             response["name"] = "error"
         else:
-            if email == '':
-                cur.execute(fetch_query_string('remove-player-account-email.sql'), {
-                    'player_id': user
-                })
+            if email == "":
+                cur.execute(
+                    fetch_query_string("remove-player-account-email.sql"),
+                    {"player_id": user},
+                )
                 response["message"] = "Removed e-mail from player account."
                 response["name"] = "success"
             else:
                 # Verify that email is unique
-                result = cur.execute(fetch_query_string('select-player-by-verified-email-address.sql'), {'email': email}).fetchall()
+                result = cur.execute(
+                    fetch_query_string("select-player-by-verified-email-address.sql"),
+                    {"email": email},
+                ).fetchall()
                 if result:
-                    response["message"] = "A player has already registered this e-mail address."
+                    response[
+                        "message"
+                    ] = "A player has already registered this e-mail address."
                     response["name"] = "error"
                     return make_response(json.jsonify(response), 400)
 
-                if existing_player_data['is_verifying_email']:
-                    response["message"] = "An e-mail address is already in the process of verification for this player.  Please wait."
+                if existing_player_data["is_verifying_email"]:
+                    response[
+                        "message"
+                    ] = "An e-mail address is already in the process of verification for this player.  Please wait."
                     response["name"] = "error"
                     return make_response(json.jsonify(response), 400)
 
-                cur.execute(fetch_query_string('update-player-account-email.sql'), {
-                    'player_id': user,
-                    'email': email
-                })
-                cur.execute(fetch_query_string('update-player-account-email-verified.sql'), {
-                    'player_id': user,
-                    'email_verified': 0,
-                })
+                cur.execute(
+                    fetch_query_string("update-player-account-email.sql"),
+                    {"player_id": user, "email": email},
+                )
+                cur.execute(
+                    fetch_query_string("update-player-account-email-verified.sql"),
+                    {"player_id": user, "email_verified": 0,},
+                )
 
                 # Send verification email (silent fail if not configured)
                 token = uuid.uuid4().hex
@@ -113,23 +131,38 @@ Complete registering your e-mail address by visiting that web page and clicking
 the "verify player" button.
 
 You can ignore this message if you didn't initiate the request.
-                """.format(token=token, DOMAIN_NAME=current_app.config.get('DOMAIN_NAME'))
+                """.format(
+                    token=token, DOMAIN_NAME=current_app.config.get("DOMAIN_NAME")
+                )
                 current_app.logger.debug(message)
-                if not current_app.config.get('DEBUG', True):
+                if not current_app.config.get("DEBUG", True):
                     try:
-                        send_message(email,
-                                     'Puzzle Massive - verify e-mail address', message, current_app.config)
+                        send_message(
+                            email,
+                            "Puzzle Massive - verify e-mail address",
+                            message,
+                            current_app.config,
+                        )
                     except Exception as err:
-                        logger.warning("Failed to send verification message. email: {email}\n {message}\n error: {err}".format(err=err, email=email, message=message))
+                        logger.warning(
+                            "Failed to send verification message. email: {email}\n {message}\n error: {err}".format(
+                                err=err, email=email, message=message
+                            )
+                        )
                         pass
 
-                cur.execute(fetch_query_string('update-player-account-email-verify-token.sql'), {
-                    'player_id': user,
-                    'email_verify_token': token,
-                    'expire_token_timeout': '+1 hour',
-                })
+                cur.execute(
+                    fetch_query_string("update-player-account-email-verify-token.sql"),
+                    {
+                        "player_id": user,
+                        "email_verify_token": token,
+                        "expire_token_timeout": "+1 hour",
+                    },
+                )
 
-                response["message"] = "Updated e-mail ({}) for player account.  ".format(email)
+                response[
+                    "message"
+                ] = "Updated e-mail ({}) for player account.  ".format(email)
                 response["name"] = "success"
 
         db.commit()
