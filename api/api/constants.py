2dd215a75ff52be1307bfb0f1eee2dc8462c1453
Jake Hickenlooper <jake@weboftomorrow.com>
2019-12-23 07:25:24 -0700

    Reformat constants.py and convertPiecesToDB.py

--- api/api/constants.py
+++ api/api/constants.py
@@ -4,48 +4,49 @@ POINTS_CAP = 15000
 USER_NAME_MAXLENGTH = 26
 EMAIL_MAXLENGTH = 254
 # The SKILL_LEVEL_RANGES is also copied in src/gallery/pm-gallery.ts
-SKILL_LEVEL_RANGES = set([
-    (0,400),
-    (400, 800),
-    (800, 1600),
-    (1600, 2200),
-    (2200, 4000),
-    (4000, 60000)
-])
+SKILL_LEVEL_RANGES = set(
+    [(0, 400), (400, 800), (800, 1600), (1600, 2200), (2200, 4000), (4000, 60000)]
+)
 
-#status
-ACTIVE           = 1   # puzzle is shown on front page if public (points distributed)
-IN_QUEUE         = 2   # in the queue of puzzles ready to be assembled (no points) only applies to public
-COMPLETED        = 3   # puzzle has been assembled
-FROZEN           = 4   # all piece movements are frozen. This can be set by the owner if private. options for a schedule or manaul triggering.
-BUGGY_UNLISTED   = 5   # Has issues with piece data
-NEEDS_MODERATION = 0   # puzzle image has just been uploaded and has yet to be seen
-FAILED_LICENSE   = -1  # image fails to show copyright information
-NO_ATTRIBUTION   = -2  # image doesn't show original author
-REBUILD          = -3  # puzzle has been placed in rebuild queue
-IN_RENDER_QUEUE  = -5  # puzzle image has been accepted and is now in render queue
-RENDERING        = -6  # puzzle is currently being rendered
+# status
+ACTIVE = 1  # puzzle is shown on front page if public (points distributed)
+IN_QUEUE = 2  # in the queue of puzzles ready to be assembled (no points) only applies to public
+COMPLETED = 3  # puzzle has been assembled
+FROZEN = 4  # all piece movements are frozen. This can be set by the owner if private. options for a schedule or manaul triggering.
+BUGGY_UNLISTED = 5  # Has issues with piece data
+NEEDS_MODERATION = 0  # puzzle image has just been uploaded and has yet to be seen
+FAILED_LICENSE = -1  # image fails to show copyright information
+NO_ATTRIBUTION = -2  # image doesn't show original author
+REBUILD = -3  # puzzle has been placed in rebuild queue
+IN_RENDER_QUEUE = -5  # puzzle image has been accepted and is now in render queue
+RENDERING = -6  # puzzle is currently being rendered
 RENDERING_FAILED = -7  # puzzle had error with being rendered
-DELETED_LICENSE  = -10 # permission to use image does not comply
-DELETED_INAPT    = -11 # inappropiate image for puzzle due to content of image
-DELETED_OLD      = -12 # out with the old
-DELETED_REQUEST  = -13 # deleted on request from the owner. Can be deleted automatically after completion or date or?
-SUGGESTED        = -20 # No image has been set; only a link to source image
-SUGGESTED_DONE   = -21 # Suggested image has been processed
+DELETED_LICENSE = -10  # permission to use image does not comply
+DELETED_INAPT = -11  # inappropiate image for puzzle due to content of image
+DELETED_OLD = -12  # out with the old
+DELETED_REQUEST = (
+    -13
+)  # deleted on request from the owner. Can be deleted automatically after completion or date or?
+SUGGESTED = -20  # No image has been set; only a link to source image
+SUGGESTED_DONE = -21  # Suggested image has been processed
 
-#permission
-PUBLIC           = 0   # Is listed on the site
-PRIVATE          = -1  # Only shows for the owner of the puzzle, can still be played by anyone with the link.
+# permission
+PUBLIC = 0  # Is listed on the site
+PRIVATE = (
+    -1
+)  # Only shows for the owner of the puzzle, can still be played by anyone with the link.
 
 # Puzzle Variant Slug names
-CLASSIC = 'classic' # initial_puzzle_variant.sql
+CLASSIC = "classic"  # initial_puzzle_variant.sql
 
 # queue levels for puzzles that have IN_QUEUE status
-QUEUE_WINNING_BID   = 1  # Player has placed this in front of all other puzzles within skill range (winning bid cost is computed from count of puzzles in bumped)
-QUEUE_BUMPED_BID    = 2  # When another puzzle that was the WINNING_BID is bumped out
-QUEUE_REBUILD       = 5  # Set when a puzzle is rebuilt by player
-QUEUE_NEW           = 6  # Set when a puzzle is created from a new uploaded image
-QUEUE_INACTIVE      = 10 # When a puzzle that was ACTIVE gets retired to IN_QUEUE because of old m_date
-QUEUE_END_OF_LINE   = 99 # Set when a puzzle is completed. Anything beyond this is also considered end of line and should be sorted by m_date
+QUEUE_WINNING_BID = 1  # Player has placed this in front of all other puzzles within skill range (winning bid cost is computed from count of puzzles in bumped)
+QUEUE_BUMPED_BID = 2  # When another puzzle that was the WINNING_BID is bumped out
+QUEUE_REBUILD = 5  # Set when a puzzle is rebuilt by player
+QUEUE_NEW = 6  # Set when a puzzle is created from a new uploaded image
+QUEUE_INACTIVE = (
+    10  # When a puzzle that was ACTIVE gets retired to IN_QUEUE because of old m_date
+)
+QUEUE_END_OF_LINE = 99  # Set when a puzzle is completed. Anything beyond this is also considered end of line and should be sorted by m_date
 
 BID_COST_PER_PUZZLE = 100
--- api/api/jobs/convertPiecesToDB.py
+++ api/api/jobs/convertPiecesToDB.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+
 # This job should be ran by a janitor worker.  It should find all puzzles in
 # redis that haven't had any activity in the last week or so.
 
@@ -20,17 +21,20 @@ from api.tools import loadConfig, deletePieceDataFromRedis
 config_file = sys.argv[1]
 config = loadConfig(config_file)
 
-db_file = config['SQLITE_DATABASE_URI']
+db_file = config["SQLITE_DATABASE_URI"]
 db = sqlite3.connect(db_file)
 
-redisConnection = redis.from_url('redis://localhost:6379/0/', decode_responses=True)
+redisConnection = redis.from_url("redis://localhost:6379/0/", decode_responses=True)
+
 
 def transfer(puzzle):
-    print('transferring puzzle: {0}'.format(puzzle))
+    print("transferring puzzle: {0}".format(puzzle))
     cur = db.cursor()
 
     query = """select * from Puzzle where (id = :puzzle)"""
-    (result, col_names) = rowify(cur.execute(query, {'puzzle': puzzle}).fetchall(), cur.description)
+    (result, col_names) = rowify(
+        cur.execute(query, {"puzzle": puzzle}).fetchall(), cur.description
+    )
     if not result:
         # Most likely because of a database switch and forgot to run this script
         # between those actions.
@@ -40,7 +44,9 @@ def transfer(puzzle):
     puzzle_data = result[0]
 
     query = """select * from Piece where (puzzle = :puzzle)"""
-    (all_pieces, col_names) = rowify(cur.execute(query, {'puzzle': puzzle}).fetchall(), cur.description)
+    (all_pieces, col_names) = rowify(
+        cur.execute(query, {"puzzle": puzzle}).fetchall(), cur.description
+    )
 
     query_update_piece = """
     update Piece set x = :x, y = :y, r = :r, parent = :parent, status = :status where puzzle = :puzzle and id = :id
@@ -48,15 +54,15 @@ def transfer(puzzle):
 
     # Save the redis data to the db
     for piece in all_pieces:
-        pieceFromRedis = redisConnection.hgetall('pc:{puzzle}:{id}'.format(**piece))
+        pieceFromRedis = redisConnection.hgetall("pc:{puzzle}:{id}".format(**piece))
 
         # The redis data may be empty so fall back on what is in db.
-        piece['x'] = pieceFromRedis.get('x', piece['x'])
-        piece['y'] = pieceFromRedis.get('y', piece['y'])
-        piece['r'] = pieceFromRedis.get('r', piece['r'])
-        piece['parent'] = pieceFromRedis.get('g', None)
+        piece["x"] = pieceFromRedis.get("x", piece["x"])
+        piece["y"] = pieceFromRedis.get("y", piece["y"])
+        piece["r"] = pieceFromRedis.get("r", piece["r"])
+        piece["parent"] = pieceFromRedis.get("g", None)
         # status is reset here based on what was in redis
-        piece['status'] = pieceFromRedis.get('s', None)
+        piece["status"] = pieceFromRedis.get("s", None)
         cur.execute(query_update_piece, piece)
 
     db.commit()
@@ -65,43 +71,48 @@ def transfer(puzzle):
 
     cur.close()
 
+
 def transferOldest(target_memory):
 
     # No puzzles that have been modified in the last 30 minutes
     newest = int(time.time()) - (30 * 60)
 
     # Get the 10 oldest puzzles
-    puzzles = redisConnection.zrange('pcupdates', 0, 10, withscores=True)
-    #print('cycle over old puzzles: {0}'.format(puzzles))
+    puzzles = redisConnection.zrange("pcupdates", 0, 10, withscores=True)
+    # print('cycle over old puzzles: {0}'.format(puzzles))
     for (puzzle, timestamp) in puzzles:
         # There may be a chance that since this process has started that
         # a puzzle could have been updated.
-        latest_timestamp = redisConnection.zscore('pcupdates', puzzle)
+        latest_timestamp = redisConnection.zscore("pcupdates", puzzle)
 
         if latest_timestamp < newest:
-            #print('transfer: {0}'.format(puzzle))
+            # print('transfer: {0}'.format(puzzle))
             transfer(puzzle)
-            memory = redisConnection.info(section='memory')
-            #print('used_memory: {used_memory_human}'.format(**memory))
-            if memory.get('used_memory') < target_memory:
+            memory = redisConnection.info(section="memory")
+            # print('used_memory: {used_memory_human}'.format(**memory))
+            if memory.get("used_memory") < target_memory:
                 break
+
 
 def transferAll():
     # Get all puzzles
-    puzzles = redisConnection.zrange('pcupdates', 0, -1)
-    print('transferring puzzles: {0}'.format(puzzles))
+    puzzles = redisConnection.zrange("pcupdates", 0, -1)
+    print("transferring puzzles: {0}".format(puzzles))
     for puzzle in puzzles:
-        print('transfer puzzle: {0}'.format(puzzle))
+        print("transfer puzzle: {0}".format(puzzle))
         transfer(puzzle)
-        memory = redisConnection.info(section='memory')
-        print('used_memory: {used_memory_human}'.format(**memory))
+        memory = redisConnection.info(section="memory")
+        print("used_memory: {used_memory_human}".format(**memory))
+
 
 def handle_fail(job, exception, exception_func, traceback):
     # TODO: handle fail for janitor; see handle_render_fail of pieceRenderer.py
     print("Handle janitor fail {0}".format(job.args[0]))
 
 
-if __name__ == '__main__':
-    confirm = input("Transfer all puzzle data out of redis and into sqlite database? y/n\n")
-    if confirm == 'y':
+if __name__ == "__main__":
+    confirm = input(
+        "Transfer all puzzle data out of redis and into sqlite database? y/n\n"
+    )
+    if confirm == "y":
         transferAll()
