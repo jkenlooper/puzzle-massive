6ec368add9a922f689b25ece73857825d19f1685
Jake Hickenlooper <jake@weboftomorrow.com>
2019-12-25 12:19:52 -0700

    Reduce timeout for refresh puzzle message
    This message may occur more often when an active puzzle is put in
    maintenance mode.  The timeout for it has been reduced to 5 seconds.

--- api/api/publish.py
+++ api/api/publish.py
@@ -161,9 +161,9 @@ class PuzzlePieceTokenView(MethodView):
             # 400 if puzzle does not exist or piece is not found
             # Only puzzles in ACTIVE state can be mutated
             err_msg = {
-                "msg": "puzzle pieces can't be moved. Please refresh.",
+                "msg": "puzzle pieces can't be moved at this time. Please wait or reload the page.",
                 "type": "puzzleimmutable",
-                "timeout": 300,
+                "timeout": 5,
             }
             cur.close()
             return make_response(encoder.encode(err_msg), 400)
