d3d2d1f87f511da8fa748e2c03ffa889f45bdfcc
Jake Hickenlooper <jake@weboftomorrow.com>
2019-12-27 07:13:08 -0700

    Add usage and options for backup-db script

--- bin/backup-db.sh
+++ bin/backup-db.sh
@@ -1,16 +1,54 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+function usage {
+  cat <<USAGE
+Usage: ${0} [-h] [-d <backup-directory-path>] [<backup-filename>]
+
+Options:
+  -h            Show help
+  -d directory  Set the directory to store backup files in [default: ./]
+
+Creates a backup file of the database.  It first converts the redis piece data
+to the database and runs any necessary scheduler tasks for batched redis data.
+Uses today's date when creating the backup file if not specified.
+USAGE
+  exit 0;
+}
+
+BACKUP_DIRECTORY=$(pwd)
+while getopts ":hd:" opt; do
+  case ${opt} in
+    h )
+      usage;
+      ;;
+    d )
+      BACKUP_DIRECTORY=${OPTARG};
+      ;;
+    \? )
+      usage;
+      ;;
+  esac;
+done;
+shift "$((OPTIND-1))";
+
 SQLITE_DATABASE_URI=$(./bin/site-cfg.py site.cfg SQLITE_DATABASE_URI);
+
+DBOWNER=$(stat -c '%U' "${SQLITE_DATABASE_URI}")
+if test "${USER}" \!= "${DBOWNER}"; then
+  echo "This should be run with the same user that owns the db file at ${SQLITE_DATABASE_URI}."
+  echo "For example: sudo su ${DBOWNER}"
+  exit 1;
+fi
 
 echo "Converting pieces to DB from Redis...";
 
-python api/api/jobs/convertPiecesToDB.py site.cfg || exit 1;
+./bin/python api/api/jobs/convertPiecesToDB.py site.cfg || exit 1;
 
 echo "Running one-off scheduler tasks to clean up any batched data";
-python api/api/scheduler.py site.cfg UpdatePlayer || exit 1;
-python api/api/scheduler.py site.cfg UpdatePuzzleStats || exit 1;
-python api/api/scheduler.py site.cfg UpdateModifiedDateOnPuzzle || exit 1;
+./bin/python api/api/scheduler.py site.cfg UpdatePlayer || exit 1;
+./bin/python api/api/scheduler.py site.cfg UpdatePuzzleStats || exit 1;
+./bin/python api/api/scheduler.py site.cfg UpdateModifiedDateOnPuzzle || exit 1;
 
 # Allow passing in a file path of where to save the db dump file
 if [ -n "${1-}" ]; then
@@ -20,9 +58,9 @@ DBDUMPFILE="db-$(date --iso-8601 --utc).dump.gz";
 fi;
 
 echo "";
-echo "Creating db dump: $DBDUMPFILE";
-echo '.dump' | sqlite3 "$SQLITE_DATABASE_URI" | gzip -c > "$DBDUMPFILE";
+echo "Creating db dump: ${BACKUP_DIRECTORY}/${DBDUMPFILE}";
+echo '.dump' | sqlite3 "$SQLITE_DATABASE_URI" | gzip -c > "${BACKUP_DIRECTORY}/${DBDUMPFILE}";
 
 echo "";
 echo "To reconstruct backup";
-echo "zcat $DBDUMPFILE | sqlite3 $SQLITE_DATABASE_URI";
+echo "zcat ${BACKUP_DIRECTORY}/${DBDUMPFILE} | sqlite3 $SQLITE_DATABASE_URI";
