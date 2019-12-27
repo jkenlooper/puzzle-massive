6b89871baf9e92ba416993634ffea3bcd775bd73
Jake Hickenlooper <jake@weboftomorrow.com>
2019-12-26 20:57:35 -0700

    Reformat script.py

--- api/api/script.py
+++ api/api/script.py
@@ -16,47 +16,52 @@ Subcommands:
 from __future__ import print_function
 from __future__ import absolute_import
 from docopt import docopt
-#from ConfigParser import RawConfigParser
+
+# from ConfigParser import RawConfigParser
 
 from .app import make_app
 from api.tools import loadConfig
 
+
 def main():
     ""
-    args = docopt(__doc__, version='0.0')
-    config_file = args['--config']
+    args = docopt(__doc__, version="0.0")
+    config_file = args["--config"]
 
     appconfig = loadConfig(config_file)
-    cookie_secret = appconfig.get('SECURE_COOKIE_SECRET')
+    cookie_secret = appconfig.get("SECURE_COOKIE_SECRET")
 
-    if args['run']:
+    if args["run"]:
         run(config_file, cookie_secret=cookie_secret)
 
-    if args['serve']:
+    if args["serve"]:
         serve(config_file, cookie_secret=cookie_secret)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
+
 
 def run(config, cookie_secret):
     "Start the web server in the foreground. Don't use for production."
     app = make_app(config=config, cookie_secret=cookie_secret)
 
     app.run(
-            host=app.config.get('HOSTAPI'),
-            port=app.config.get('PORTAPI'),
-            use_reloader=True,
-            )
+        host=app.config.get("HOSTAPI"),
+        port=app.config.get("PORTAPI"),
+        use_reloader=True,
+    )
+
 
 def serve(config, cookie_secret):
     from gevent import pywsgi
 
     app = make_app(config=config, cookie_secret=cookie_secret)
 
-    host = app.config.get('HOSTAPI')
-    port = app.config.get('PORTAPI')
+    host = app.config.get("HOSTAPI")
+    port = app.config.get("PORTAPI")
 
-    print(u'serving on {host}:{port}'.format(**locals()))
+    print(u"serving on {host}:{port}".format(**locals()))
     server = pywsgi.WSGIServer((host, port), app)
 
     server.serve_forever(stop_timeout=10)
