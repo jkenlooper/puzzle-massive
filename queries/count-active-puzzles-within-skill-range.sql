521ba20c6dc9799f4e1b7cff74b96afb471585d8
Jake Hickenlooper <jake@weboftomorrow.com>
2020-01-10 15:40:19 -0700

    Fix query for moving puzzles to active

--- queries/count-active-puzzles-within-skill-range.sql
+++ queries/count-active-puzzles-within-skill-range.sql
@@ -3,5 +3,6 @@ select count(distinct p.id) as active_count
 from Puzzle as p
 join PuzzleInstance as pi on (p.id = pi.original)
 where p.status = 1 -- ACTIVE
+and p.permission = 0 -- PUBLIC
 and p.pieces >= :low and p.pieces < :high
 ;
--- queries/update-puzzle-next-in-queue-to-be-active.sql
+++ queries/update-puzzle-next-in-queue-to-be-active.sql
@@ -1,6 +1,7 @@
 update Puzzle set status = 1, -- ACTIVE
 m_date = datetime('now', '-4 days')
 where status = 2 -- IN_QUEUE
+and permission = 0 -- PUBLIC
 and pieces >= :low and pieces < :high
 and id in (
     select distinct p.id
@@ -14,6 +15,7 @@ limit :active_count - (
     from Puzzle as p
     join PuzzleInstance as pi on (p.id = pi.original)
     where p.status = 1 -- ACTIVE
+    and p.permission = 0 -- PUBLIC
     and p.pieces >= :low and p.pieces < :high
 )
 ;
