70c1be03a642b28c0de303b2357e210ed9ff63d3
Jake Hickenlooper <jake@weboftomorrow.com>
2020-01-16 07:23:24 -0700

    Record latency for players
    The latency value returned to the player for the pong reply could be
    shown.  The latency of other players isn't really useful for all players
    to see.  For the admin side, the overall latency (last 1000 entries)
    might be useful to show somewhere.

--- api/api/ping.py
+++ api/api/ping.py
@@ -12,11 +12,19 @@ from api.database import fetch_query_string, rowify
 from api.constants import ACTIVE
 
 # Set ping token expire to be a little over 5 minutes
-PING_TOKEN_EXPIRE = 300 + 60
+PING_EXPIRE = 300 + 60
 
 
-def get_pingtoken_key(puzzle):
-    return "pingtoken:{puzzle}".format(puzzle=puzzle)
+def get_pingtoken_key(puzzle, user, token):
+    "Store the ping timestamp value that expires within a minute."
+    return "pingtoken:{puzzle}:{user}:{token}".format(
+        puzzle=puzzle, user=user, token=token
+    )
+
+
+def get_ping_key(puzzle):
+    "sorted set for recording the reply timestamps for players pinging a puzzle"
+    return "ping:{puzzle}".format(puzzle=puzzle)
 
 
 class PingPuzzleView(MethodView):
@@ -61,12 +69,8 @@ class PingPuzzleView(MethodView):
         # publish to the puzzle channel the ping with the user id.  This will
         # allow that player to determine their latency.
         token = uuid.uuid4().hex[:4]
-        # TODO: record token in redis for a minute
-        pingtoken_key = get_pingtoken_key(puzzle)
-        redis_connection.zadd(
-            pingtoken_key, {"{user}:{token}".format(user=user, token=token): now_ms}
-        )
-        redis_connection.expire(pingtoken_key, PING_TOKEN_EXPIRE)
+        pingtoken_key = get_pingtoken_key(puzzle, user, token)
+        redis_connection.setex(pingtoken_key, 60, now_ms)
 
         current_app.logger.debug("publish ping {puzzle_id}".format(puzzle_id=puzzle_id))
         sse.publish(
@@ -127,24 +131,32 @@ class PingPuzzleView(MethodView):
                 response["name"] = "invalid"
                 return make_response(json.jsonify(response), 200)
 
-        pingtoken_key = get_pingtoken_key(puzzle)
-        ping_start = redis_connection.zscore(
-            pingtoken_key, "{user}:{token}".format(user=user, token=token)
-        )
+        # Determine latency for the player and record timestamp in sorted set.
+        pingtoken_key = get_pingtoken_key(puzzle, user, token)
+        ping_start = redis_connection.get(pingtoken_key)
+        redis_connection.delete(pingtoken_key)
         ping_end = int(time.time() * 1000)
         if not ping_start:
             response["message"] = "Ignoring error when determining latency."
             response["name"] = "ignored"
             return make_response(json.jsonify(response), 200)
+        ping_start = int(ping_start)
+        ping_key = get_ping_key(puzzle)
+        redis_connection.zadd(ping_key, {user: ping_end})
+        redis_connection.expire(ping_key, PING_EXPIRE)
 
         latency = ping_end - ping_start
 
-        # Publish to the puzzle channel the latency with the user id.
-        sse.publish(
-            "{user}:{latency}".format(user=user, latency=latency),
-            type="latency",
-            channel=puzzle_id,
+        # Record the latency for the player
+        redis_connection.lpush(
+            "latency",
+            "{user}:{timestamp}:{latency}".format(
+                user=user, timestamp=ping_end, latency=latency
+            ),
         )
+        # Keep only the last 1000 entries to latency
+        redis_connection.ltrim("latency", 0, 999)
+
         response["message"] = "Latency"
         response["data"]["latency"] = latency
         response["name"] = "success"
