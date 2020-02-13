420e16fa2c4b05b1549b1e9592e339ffc5318d9c
Jake Hickenlooper <jake@weboftomorrow.com>
2020-02-13 07:01:31 -0700

    Reformat

--- api/api/instance.py
+++ api/api/instance.py
@@ -1,4 +1,4 @@
-#/newapi/puzzle-instance/
+# /newapi/puzzle-instance/
 import os
 
 from flask import current_app, redirect, make_response, abort, request
@@ -29,6 +29,7 @@ class CreatePuzzleInstanceView(MethodView):
     The player needs to have a minimum of 2400 dots
     An available puzzle instance slot
     """
+
     decorators = [user_not_banned]
 
     def post(self):
@@ -38,53 +39,62 @@ class CreatePuzzleInstanceView(MethodView):
 
         # Check pieces arg
         try:
-            pieces = int(args.get('pieces', current_app.config['MINIMUM_PIECE_COUNT']))
+            pieces = int(args.get("pieces", current_app.config["MINIMUM_PIECE_COUNT"]))
         except ValueError as err:
             abort(400)
-        if pieces < current_app.config['MINIMUM_PIECE_COUNT']:
+        if pieces < current_app.config["MINIMUM_PIECE_COUNT"]:
             abort(400)
 
-        bg_color = check_bg_color(args.get('bg_color', '#808080')[:50])
+        bg_color = check_bg_color(args.get("bg_color", "#808080")[:50])
 
         # Check puzzle_id
-        original_puzzle_id = args.get('puzzle_id')
-        if (not original_puzzle_id):
+        original_puzzle_id = args.get("puzzle_id")
+        if not original_puzzle_id:
             abort(400)
 
-
         # Check permission
-        permission = int(args.get('permission', PUBLIC))
+        permission = int(args.get("permission", PUBLIC))
         if permission not in (PUBLIC, PRIVATE):
             abort(400)
 
-        user = int(current_app.secure_cookie.get(u'user') or user_id_from_ip(request.headers.get('X-Real-IP')))
+        user = int(
+            current_app.secure_cookie.get(u"user")
+            or user_id_from_ip(request.headers.get("X-Real-IP"))
+        )
 
         cur = db.cursor()
 
         # The user should have
         # 2400 or more dots (points)
         # TODO: this could be configurable per site or for other reasons.
-        #userHasEnoughPoints = cur.execute(fetch_query_string("select-minimum-points-for-user.sql"), {'user': user, 'points': 2400}).fetchall()
-        #if not userHasEnoughPoints:
+        # userHasEnoughPoints = cur.execute(fetch_query_string("select-minimum-points-for-user.sql"), {'user': user, 'points': 2400}).fetchall()
+        # if not userHasEnoughPoints:
         #    abort(400)
 
         # An available instance slot
-        result = cur.execute(fetch_query_string("select-available-user-puzzle-slot-for-player.sql"), {'player': user}).fetchone()[0]
+        result = cur.execute(
+            fetch_query_string("select-available-user-puzzle-slot-for-player.sql"),
+            {"player": user},
+        ).fetchone()[0]
         userHasAvailablePuzzleInstanceSlot = bool(result)
         if not userHasAvailablePuzzleInstanceSlot:
             abort(400)
 
         # Check if puzzle is valid to be a new puzzle instance
-        result = cur.execute(fetch_query_string("select-valid-puzzle-for-new-puzzle-instance.sql"), {
-            'puzzle_id': original_puzzle_id,
-            'ACTIVE': ACTIVE,
-            'IN_QUEUE': IN_QUEUE,
-            'COMPLETED': COMPLETED,
-            'FROZEN': FROZEN,
-            'REBUILD': REBUILD,
-            'IN_RENDER_QUEUE': IN_RENDER_QUEUE,
-            'RENDERING': RENDERING,
-            'PUBLIC': PUBLIC}).fetchall()
+        result = cur.execute(
+            fetch_query_string("select-valid-puzzle-for-new-puzzle-instance.sql"),
+            {
+                "puzzle_id": original_puzzle_id,
+                "ACTIVE": ACTIVE,
+                "IN_QUEUE": IN_QUEUE,
+                "COMPLETED": COMPLETED,
+                "FROZEN": FROZEN,
+                "REBUILD": REBUILD,
+                "IN_RENDER_QUEUE": IN_RENDER_QUEUE,
+                "RENDERING": RENDERING,
+                "PUBLIC": PUBLIC,
+            },
+        ).fetchall()
         if not result:
             # Puzzle does not exist or is not a valid puzzle to create instance from.
             abort(400)
@@ -92,23 +102,26 @@ class CreatePuzzleInstanceView(MethodView):
         (result, col_names) = rowify(result, cur.description)
         originalPuzzleData = result[0]
 
-        puzzle_id = generate_new_puzzle_id(originalPuzzleData['name'])
+        puzzle_id = generate_new_puzzle_id(originalPuzzleData["name"])
 
         # Create puzzle dir
-        puzzle_dir = os.path.join(current_app.config.get('PUZZLE_RESOURCES'), puzzle_id)
+        puzzle_dir = os.path.join(current_app.config.get("PUZZLE_RESOURCES"), puzzle_id)
         os.mkdir(puzzle_dir)
 
-        d = {'puzzle_id':puzzle_id,
-            'pieces':pieces,
-            'name':originalPuzzleData['name'],
-            'link':originalPuzzleData['link'],
-            'description':originalPuzzleData['description'],
-            'bg_color':bg_color,
-            'owner':user,
-            'queue':QUEUE_NEW,
-            'status': IN_RENDER_QUEUE,
-            'permission':permission}
-        cur.execute("""insert into Puzzle (
+        d = {
+            "puzzle_id": puzzle_id,
+            "pieces": pieces,
+            "name": originalPuzzleData["name"],
+            "link": originalPuzzleData["link"],
+            "description": originalPuzzleData["description"],
+            "bg_color": bg_color,
+            "owner": user,
+            "queue": QUEUE_NEW,
+            "status": IN_RENDER_QUEUE,
+            "permission": permission,
+        }
+        cur.execute(
+            """insert into Puzzle (
         puzzle_id,
         pieces,
         name,
@@ -129,29 +142,48 @@ class CreatePuzzleInstanceView(MethodView):
         :queue,
         :status,
         :permission);
-        """, d)
+        """,
+            d,
+        )
         db.commit()
 
-        result = cur.execute("select * from Puzzle where puzzle_id = :puzzle_id;", {'puzzle_id': puzzle_id}).fetchall()
+        result = cur.execute(
+            "select * from Puzzle where puzzle_id = :puzzle_id;",
+            {"puzzle_id": puzzle_id},
+        ).fetchall()
         if not result:
             abort(500)
 
         (result, col_names) = rowify(result, cur.description)
         puzzleData = result[0]
-        puzzle = puzzleData['id']
+        puzzle = puzzleData["id"]
 
-        classic_variant = cur.execute(fetch_query_string("select-puzzle-variant-id-for-slug.sql"), {"slug": CLASSIC}).fetchone()[0]
-        cur.execute(fetch_query_string("insert-puzzle-instance.sql"), {"original": originalPuzzleData['id'], "instance": puzzle, "variant": classic_variant})
+        classic_variant = cur.execute(
+            fetch_query_string("select-puzzle-variant-id-for-slug.sql"),
+            {"slug": CLASSIC},
+        ).fetchone()[0]
+        cur.execute(
+            fetch_query_string("insert-puzzle-instance.sql"),
+            {
+                "original": originalPuzzleData["id"],
+                "instance": puzzle,
+                "variant": classic_variant,
+            },
+        )
 
-        cur.execute(fetch_query_string("fill-user-puzzle-slot.sql"), {'player': user, 'puzzle': puzzle})
+        cur.execute(
+            fetch_query_string("fill-user-puzzle-slot.sql"),
+            {"player": user, "puzzle": puzzle},
+        )
 
         db.commit()
         cur.close()
 
         job = current_app.createqueue.enqueue_call(
-            func='api.jobs.pieceRenderer.render', args=([puzzleData]), result_ttl=0,
-            timeout='24h'
+            func="api.jobs.pieceRenderer.render",
+            args=([puzzleData]),
+            result_ttl=0,
+            timeout="24h",
         )
 
-
-        return redirect('/chill/site/puzzle/{0}/'.format(puzzle_id), code=303)
+        return redirect("/chill/site/puzzle/{0}/".format(puzzle_id), code=303)
--- api/api/render.py
+++ api/api/render.py
@@ -5,15 +5,13 @@ from flask.views import MethodView
 
 from api.app import db
 from .database import read_query_file, rowify
-from api.constants import (
-        IN_RENDER_QUEUE,
-        REBUILD
-        )
+from api.constants import IN_RENDER_QUEUE, REBUILD
 
 
 class RenderPuzzlesView(MethodView):
     """
     """
+
     def get(self):
         "Route is protected by basic auth in nginx"
         # TODO: Check user to see if role matches?
@@ -22,21 +20,32 @@ class RenderPuzzlesView(MethodView):
         #     abort(403)
 
         cur = db.cursor()
-        puzzles = rowify(cur.execute(read_query_file("select-puzzles-in-render-queue.sql"),
-            {'IN_RENDER_QUEUE': IN_RENDER_QUEUE,
-             'REBUILD': REBUILD})
-            .fetchall(), cur.description)[0]
+        puzzles = rowify(
+            cur.execute(
+                read_query_file("select-puzzles-in-render-queue.sql"),
+                {"IN_RENDER_QUEUE": IN_RENDER_QUEUE, "REBUILD": REBUILD},
+            ).fetchall(),
+            cur.description,
+        )[0]
         print("found {0} puzzles to render".format(len(puzzles)))
 
         # push each puzzle to artist job queue
         for puzzle in puzzles:
             job = current_app.createqueue.enqueue_call(
-                func='api.jobs.pieceRenderer.render', args=([puzzle]), result_ttl=0,
-                timeout='24h'
+                func="api.jobs.pieceRenderer.render",
+                args=([puzzle]),
+                result_ttl=0,
+                timeout="24h",
             )
 
-        response = make_response("""
+        response = make_response(
+            """
         Starting render job.
         <a href="{url}">{url}</a>
-        """.format(url=request.url), 202)
+        """.format(
+                url=request.url
+            ),
+            202,
+        )
+        cur.close()
         return response
