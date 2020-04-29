63767c3501a0503b7b352909fb1e47377eee6a16
Jake Hickenlooper <jake@weboftomorrow.com>
2020-04-29 07:52:08 -0600

    Clean up logo in header and footer

---
+++ source-media/logo-puzzle-massive/README.md
@@ -0,0 +1,2 @@
+Logo font is "Giant Head OT" by Jess Latham - Blue Vinyl Fonts
+[Hosted on Font Squirrel](https://www.fontsquirrel.com/license/Giant-Head-OT)
--- source-media/logo-puzzle-massive/_images.mk
+++ source-media/logo-puzzle-massive/_images.mk
@@ -31,3 +31,28 @@ media/puzzle-massive-logo-darkblue-on-black--440.png : source-media/logo-puzzle-
 
 media/puzzle-massive-logo-blue-on-white--440.png : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-white.png
 	convert $< -format png -resize 440x $@;
+
+media/puzzle-massive-logo-tertiary-on-background-accent--122.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-black.png
+	convert $< -format jpg -background "#F3EDFC" -flatten -alpha off -resize 122x $@;
+media/puzzle-massive-logo-tertiary-on-background-accent--272.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-black.png
+	convert $< -format jpg -background "#F3EDFC" -flatten -alpha off -resize 272x $@;
+media/puzzle-massive-logo-tertiary-on-background-accent--544.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-black.png
+	convert $< -format jpg -background "#F3EDFC" -flatten -alpha off -resize 544x $@;
+
+media/puzzle-massive-logo-tertiary-on-dark--520.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 520x $@;
+media/puzzle-massive-logo-tertiary-on-dark--731.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 731x $@;
+media/puzzle-massive-logo-tertiary-on-dark--1040.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 1040x $@;
+media/puzzle-massive-logo-tertiary-on-dark--1462.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-blue-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 1462x $@;
+
+media/puzzle-massive-logo-white-on-dark--520.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-white-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 520x $@;
+media/puzzle-massive-logo-white-on-dark--731.jpg :  source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-white-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 731x $@;
+media/puzzle-massive-logo-white-on-dark--1040.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-white-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 1040x $@;
+media/puzzle-massive-logo-white-on-dark--1462.jpg : source-media/logo-puzzle-massive/puzzle-massive-logo-mosaic/puzzle-massive-logo-mosaic-white-on-black.png
+	convert $< -format jpg -background "#000105" -flatten -alpha off -resize 1462x $@;
