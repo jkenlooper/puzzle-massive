ac19870f9e5c5b7c92f03d17903e9562a1e2abad
Jake Hickenlooper <jake@weboftomorrow.com>
2021-02-07 11:36:51 -0700

    Clean up

--- bin/insert-or-replace-bit-icons.py
+++ bin/insert-or-replace-bit-icons.py
@@ -5,6 +5,7 @@ import os.path
 import glob
 
 import sqlite3
+import yaml
 
 from api.tools import loadConfig
 from api.database import read_query_file
@@ -21,11 +22,13 @@ def add_bit_icons_in_file_system(db):
     insert_or_replace_bit_icon_query = read_query_file(
         "_insert_or_replace_bit_icon.sql"
     )
-    for source_file in glob.glob(os.path.join("media", "bit-icons", "source-*.yaml")):
+    for source_file in glob.glob(
+        os.path.join("source-media", "bit-icons", "source-*.yaml")
+    ):
         root = os.path.splitext(os.path.basename(source_file))[0]
         group_name = root[root.index("-") + 1 :]
         with open(source_file, "r") as f:
-            source = yaml.safe_load(source_file)
+            source = yaml.safe_load(f)
             cur.execute(
                 upsert_author_query,
                 {
@@ -39,11 +42,9 @@ def add_bit_icons_in_file_system(db):
                 select_author_id_query, {"slug_name": source["slug_name"]}
             ).fetchone()[0]
             for bit_icon_file in glob.glob(
-                os.path.join(os.path.dirname(source_file), f"64-{group_name}-*.png")
+                os.path.join(os.path.dirname(source_file), f"{group_name}-*")
             ):
-                name = os.path.basename(bit_icon_file)[len(group_name) + 4 : -4]
-                bit_icon_name = "-".join([group_name, name])
-
+                bit_icon_name = os.path.splitext(os.path.basename(bit_icon_file))[0]
                 bits.append({"name": bit_icon_name, "author": author_id})
 
     def each(bit):
