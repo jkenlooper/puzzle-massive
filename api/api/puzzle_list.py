1cdf1e64ef1a8575b29015d0f48d080223bd3047
Jake Hickenlooper <jake@weboftomorrow.com>
2021-07-15 08:04:14 -0600

    Remove user_not_banned restriction on puzzle list

--- api/api/puzzle_list.py
+++ api/api/puzzle_list.py
@@ -121,8 +121,6 @@ def build_select_available_puzzle_sql(query_file, status, type):
 class PuzzleListView(MethodView):
     """"""
 
-    decorators = [user_not_banned]
-
     def get(self):
         """
         /newapi/puzzle-list/?status=...&status=...&
